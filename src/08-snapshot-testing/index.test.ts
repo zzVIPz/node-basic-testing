de65873125894156823473b0693e3c920964b42b
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-02-10 02:21:11 -0500

    add snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,27 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const expectedMatch = {
+    next: {
+      next: {
+        next: {
+          next: null,
+          value: null,
+        },
+        value: 3,
+      },
+      value: 2,
+    },
+    value: 1,
+  };
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toStrictEqual(expectedMatch);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toMatchSnapshot();
   });
 });
